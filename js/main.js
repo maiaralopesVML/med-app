51c47dd5c79b7d0091b8f63d32c4a5f38b0e7d9c
Maiara Lopes <maiara.lopes@wundermanthompson.com>
2025-10-09 19:53:45 +0100

    Starts skeleton of page with form and list sections built

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-console.log("JS is connected!");
-
 const app = document.getElementById("app");
-app.textContent = "Hello from JavaScript!";
+
+const header = document.createElement("header");
+const main = document.createElement("main");
+const petForm = document.createElement("section");
+const petList = document.createElement("section");
+header.id = "header";
+main.id = "main";
+petForm.id = "pet-form-section";
+petList.id = "pet-list-section";
+header.textContent = "Dog Medication Tracker";
+petForm.textContent = "Pet Form Section";
+petList.textContent = "Pet List Section";
+
+main.appendChild(petForm);
+main.appendChild(petList);
+app.appendChild(header);
+app.appendChild(main);
